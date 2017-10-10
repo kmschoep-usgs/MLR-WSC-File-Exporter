c4e5f0816aea04a1a750226b4a2d5a92946a9dc0
Andrew Yan <ayan@usgs.gov>
2017-10-10 14:47:41 -0500

    remove export directory variable from config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 PROJECT_DIR = PROJECT_DIR = os.path.dirname(__file__)
-EXPORT_DIRECTORY = os.getenv('EXPORT_DIRECTORY', 'PROJECT_DIR')
 S3_BUCKET = os.getenv('S3_BUCKET', 'mlr-exports')
 AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
 TIERNAME = os.getenv('TIERNAME', 'development')
